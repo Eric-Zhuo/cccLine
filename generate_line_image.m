def8492c736f2e60a21b715e6dd10b510a21b637
Zhuo Qing-Feng <909640172@qq.com>
2024-02-28 21:30:53 +0800

    Update generate_line_image.m

--- generate_line_image.m
+++ generate_line_image.m
@@ -15,7 +15,7 @@ function [line_image] = generate_line_image(Image_num,K,l)
       x = normrnd(0,1,3,1);
       n = x/norm(x);
       d = 0;
-      Q=[(l^2-1)*n(1)^2+(d+l*n(3))^2  (l^2-1)*n(1)*n(2)                -(l*d+n(3))*n(1);
+      Q = [(l^2-1)*n(1)^2+(d+l*n(3))^2  (l^2-1)*n(1)*n(2)                -(l*d+n(3))*n(1);
           (l^2-1)*n(1)*n(2)               (l^2-1)*(n(2))^2+(d+l*n(3))^2   -(l*d+n(3))*n(2);
           -(l*d+n(3))*n(1)                -(l*d+n(3))*n(2)                 d^2-n(3)^2      ];
       C = inv(K')*Q*inv(K);
